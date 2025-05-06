1bf7f8cf49081a63375361d1f79483a03e8ddeb6
Rick-5 <rcvc296@gmail.com>
2025-05-06 18:46:28 -0300

    adição de function para contar agendamentos com base no id do médico

--- functions.sql
+++ functions.sql
@@ -5,3 +5,21 @@ BEGIN
 	RETURN TIMESTAMPDIFF(YEAR, dt_nascimento, CURDATE());
 END $$
 DELIMITER ;
+
+-- FUNÇÃO PARA CONTAR AGENDAMENTOS COM BASE NO ID DO MÉDICO
+DELIMITER $
+
+CREATE FUNCTION contar_agendamentos_medico(p_id_medico INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    RETURN (
+        SELECT COUNT(*)
+        FROM agendamentos
+        WHERE id_medico = p_id_medico
+    );
+END $$
+
+DELIMITER ;
+
+SELECT contar_agendamentos_medico() AS total_agendamentos;
